3fdf90e8a753f5b69ddede0ce36c0535b53bfe74
Troels-dat21B <troe1912@stud.kea.dk>
2024-04-12 16:27:29 +0200

    Added basic python logic. needs to be redefined with actual info

---
+++ rabbitmq_listener.py
@@ -0,0 +1,31 @@
+import pika
+
+# RabbitMQ connection parameters
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
+RABBITMQ_USERNAME = 'guest'
+RABBITMQ_PASSWORD = 'guest'
+RABBITMQ_QUEUE = 'myqueue'
+
+def callback(ch, method, properties, body):
+    print(f"Received message: {body}")
+
+def main():
+    # Connect to RabbitMQ
+    credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+    parameters = pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    # Declare the queue
+    channel.queue_declare(queue=RABBITMQ_QUEUE)
+
+    # Set up callback to handle incoming messages
+    channel.basic_consume(queue=RABBITMQ_QUEUE, on_message_callback=callback, auto_ack=True)
+
+    # Start consuming messages
+    print("Waiting for messages. To exit press CTRL+C")
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    main()
